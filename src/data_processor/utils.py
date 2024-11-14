5084952ee8ae3ce513f2a5ca0978852c5f5047a0
narayan-shyam <narayan.s@tigeranalytics.com>
2024-11-15 00:19:09 +0530

    Fixed the black formatting issue

--- src/data_processor/utils.py
+++ src/data_processor/utils.py
@@ -1,5 +1,6 @@
+# src/data_processor/utils.py
 def calculate_metrics(df: pd.DataFrame) -> Dict:
     return {
-        'total_records': len(df),
-        'average_value': df['value'].mean() if 'value' in df else 0
+        "total_records": len(df),
+        "average_value": df["value"].mean() if "value" in df else 0,
     }
