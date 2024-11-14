15b6898d06f7a238af59348057060fbc99359d9c
narayan-shyam <narayan.s@tigeranalytics.com>
2024-11-15 00:35:25 +0530

    Changed list() to list_objects()

--- tests/integration/test_databricks_deploy.py
+++ tests/integration/test_databricks_deploy.py
@@ -1,17 +1,16 @@
 # tests/integration/test_databricks_deploy.py
 import os
-import pytest
 from databricks_cli.sdk.api_client import ApiClient
 from databricks_cli.workspace.api import WorkspaceApi
 
 def test_databricks_connection():
-    host = os.getenv('DATABRICKS_HOST')
-    token = os.getenv('DATABRICKS_TOKEN')
-    
-    api_client = ApiClient(
-        host=host,
-        token=token
-    )
-    workspace_api = WorkspaceApi(api_client)
-    workspaces = workspace_api.list("/")
-    assert workspaces is not None
+   host = os.getenv('DATABRICKS_HOST')
+   token = os.getenv('DATABRICKS_TOKEN')
+   
+   api_client = ApiClient(
+       host=host,
+       token=token
+   )
+   workspace_api = WorkspaceApi(api_client)
+   workspaces = workspace_api.list_objects("/")
+   assert workspaces is not None
