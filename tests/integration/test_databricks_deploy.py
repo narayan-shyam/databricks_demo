1a6a154bdf68914c0bf8747ac2052562ec2348c6
narayan-shyam <narayan.s@tigeranalytics.com>
2024-11-15 00:33:00 +0530

    Fixed test_databricks_deploy.py to use ENV variables

--- tests/integration/test_databricks_deploy.py
+++ tests/integration/test_databricks_deploy.py
@@ -1,12 +1,16 @@
 # tests/integration/test_databricks_deploy.py
+import os
 import pytest
 from databricks_cli.sdk.api_client import ApiClient
 from databricks_cli.workspace.api import WorkspaceApi
 
-def test_databricks_connection(databricks_token, databricks_host):
+def test_databricks_connection():
+    host = os.getenv('DATABRICKS_HOST')
+    token = os.getenv('DATABRICKS_TOKEN')
+    
     api_client = ApiClient(
-        host=databricks_host,
-        token=databricks_token
+        host=host,
+        token=token
     )
     workspace_api = WorkspaceApi(api_client)
     workspaces = workspace_api.list("/")
